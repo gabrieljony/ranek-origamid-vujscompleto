818bffd588797ed217f8fcf31dffac18ebed2fb8
Gabriel Jony <gabjony@gmail.com>
2020-05-16 07:10:12 -0300

    Busca

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,9 +1,23 @@
 <template>
-  <div>Produtos Buscar</div>
+  <form>
+    <input name="busca" id="busca" type="text" v-model="busca" />
+    <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos" />
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      busca: ""
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({ query: { q: this.busca } });
+    }
+  }
+};
 </script>
 
-<style scoped></style>
+<style></style>
