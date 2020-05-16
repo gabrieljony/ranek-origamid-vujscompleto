889c2c804b639590383a0bfbb16d9f197e4fa278
Gabriel Jony <gabjony@gmail.com>
2020-05-16 06:56:53 -0300

    Lista de produtos, buscando da api falsa

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>Produtos Lista</div>
+  <section class="produtos-container">
+    <div v-for="produto in produtos" :key="produto.id">
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      />
+      <p class="preco">{{ produto.preco }}</p>
+      <h2 class="titulo">{{ produto.nome }}</h2>
+      <p>{{ produto.descricao }}</p>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import { api } from "@/services";
+
+export default {
+  data() {
+    return {
+      produtos: null,
+    };
+  },
+  methods: {
+    getProdutos() {
+      api.get("/produto").then((response) => {
+        this.produtos = response.data;
+      });
+    },
+  },
+  created() {
+    this.getProdutos();
+  },
+};
 </script>
 
 <style scoped></style>
