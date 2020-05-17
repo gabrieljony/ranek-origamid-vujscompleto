2242a13dc01b8a12ab40e08406d528a68a7662d9
Gabriel Jony <gabjony@gmail.com>
2020-05-17 09:33:59 -0300

    Vuex Formulário

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -5,3 +5,20 @@ export function serialize(obj) {
   }
   return queryString;
 }
+
+// existe alguns plugins para retornar o get e set no vuex, mas foi criado esse basico, sem precisar instalar lib
+export function mapFields(options) {
+  const object = {};
+  for (let x = 0; x < options.fields.length; x++) {
+    const field = [options.fields[x]];
+    object[field] = {
+      get() {
+        return this.$store.state[options.base][field];
+      },
+      set(value) {
+        this.$store.commit(options.mutation, { [field]: value });
+      }
+    };
+  }
+  return object;
+}
